b148dbc96461d47b69008d90caf32b9e6466d0a6
Gitea <gitea@fake.local>
2025-06-05 23:25:41 +0300

    export işlemleri

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  //distDir: 'docs',
+}
+ 
+module.exports = nextConfig
